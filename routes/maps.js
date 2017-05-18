970c60bbd997a8ba18b0260d4656b32f656d72ff
Chenthill Palanisamy <chenthill.palanisamy@softwareag.com>
2017-05-18 12:03:19 +0530

    add query parameter for directions route

--- routes/maps.js
+++ routes/maps.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET map direction listing. */
 router.get('/', function(req, res, next) {
-    res.render('maps');
+    var to_location;
+    if (req.query.to != undefined) {
+        to_location = req.query.to + ",Bangalore";
+    } else {
+        to_location = "Salem, TamilNadu";
+    }
+
+    res.render('maps', {to_location: to_location});
 });
 
 module.exports = router;
